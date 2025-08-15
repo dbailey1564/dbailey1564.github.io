f7923876a9ecd4e0c1e7b2213cd43a135c7cc293
dbailey1564 <137481635+dbailey1564@users.noreply.github.com>
2025-08-15 08:10:40 -0400

    init
    initial commit. I'll setup _config.yml later

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('rollit-cache-v1').then(cache => {
+      return cache.addAll(['index.html', 'manifest.webmanifest']);
+    })
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
